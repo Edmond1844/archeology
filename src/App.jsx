ecf180e69c48e240fdcce95a49515b883e06ba96
Edmond1844 <pavel.rudenko1844@gmail.com>
2025-06-26 20:01:50 +0300

    Сделал секцию вопросов

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Hero from './components/Hero/Hero.jsx';
 import SectionExpeditions from './components/SectionExpeditions/SectionExpeditions.jsx';    
-// import SectionQuestions from './components/SectionQuestions/SectionQuestions.jsx';
+import SectionQuestions from './components/SectionQuestions/SectionQuestions.jsx';
 import SectionSubscription from './components/SectionSubscription/SectionSubscription.jsx';
 import Footer from './components/Footer/Footer.jsx';
 
 import filterButtons from './data/filterButtons.js';
 import archeologyTours from './data/tours.js';
+import questions from './data/questions.js';
 
 function App() {
     return (
@@ -13,7 +14,7 @@ function App() {
             <Hero />
             <main>
                 <SectionExpeditions bottonList={filterButtons} tours={archeologyTours}/>
-                {/* <SectionQuestions faq={faq}/> */}
+                <SectionQuestions questions={questions}/>
                 <SectionSubscription />
             </main>
             <Footer />
--- src/components/SectionQuestions/SectionQuestions.jsx
+++ src/components/SectionQuestions/SectionQuestions.jsx
@@ -1,18 +1,17 @@
-import './index.css';
+import styles from "./SectionQuestions.module.css";
 
-function SectionQuestions({faq}) {
+function SectionQuestions({questions}) {
     return (
-        <section className="section-questions">
-            <div className="section-questions__content">
-                <h3 className="section-questions__title">Часто задаваемые вопросы</h3>    
-                <p className="section-questions__text">Что чаще всего спрашивают наши клиенты о турах и услугах.</p>
+        <section className={`${styles.section_questions} container`}>
+            <div className={styles.section_questions__content}>
+                <h3 className={styles.section_questions__title}>Часто задаваемые вопросы</h3>    
+                <p className={styles.section_questions__text}>Что чаще всего спрашивают наши клиенты о турах и услугах.</p>
             </div> 
-
             {
-                faq.map((item) =>(
-                    <div className="section-questions__item" key={item.id}>
-                        <p className="section-questions__question">{item.question}</p>
-                        <p className="section-questions__answer">{item.answer}</p>
+                questions.map((questionItem) => (
+                    <div className={styles.section_questions__wrapper} key={questionItem.id}>
+                        <p className={styles.section_questions__question}>{questionItem.question}</p>
+                        <p className={styles.section_questions__answer}>{questionItem.answer}</p>
                     </div>
                 ))
             }
